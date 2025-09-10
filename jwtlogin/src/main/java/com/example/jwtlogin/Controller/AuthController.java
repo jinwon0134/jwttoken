9e943ae6002ae7d19b183f9988e09853c080ca3f
unknown <jinwon0134@gmail.com>
2025-09-10 16:49:52 +0900

    회원가입 완성

--- jwtlogin/src/main/java/com/example/jwtlogin/Controller/AuthController.java
+++ jwtlogin/src/main/java/com/example/jwtlogin/Controller/AuthController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/test/api/auth")
 public class AuthController {
 
     private final UserService userService;
